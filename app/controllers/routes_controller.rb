f4aebcf24d71046f18fba22b8b52f8083bb0cb1e
Choi.Michael <typeinmichael@gmail.com>
2014-06-06 14:37:22 -0700

    Unstuck gmap stops

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -3,15 +3,12 @@ class RoutesController < ApplicationController
   respond_to :json, :html
   def index
   	@routes = Route.all
+
     respond_with @routes
-    # @hash = Route.build_markers(@routes) do |route, marker|
-    # marker.lat route.latitude
-    # marker.lon route.longitude
-  end
   end
 
   def show
-  	@route = Route.find(params[:id])
+    @route = Route.find(params[:id])
     respond_with @route
   end
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
     @routes = Route.all
-#    @route_start = Stop.find(Route.first.route_stops.order("stop_sequence ASC").map(&:stop_id))
+   # @route_start = Stop.find(Route.first.route_stops.order("stop_sequence ASC").map(&:stop_id))
 
 #    @route_end = Route.first.route_stops.order("stop_sequence ASC").last.stop
-	
-	@routefirst_lat = Route.first.stops.first.stop_lat
-	@routefirst_lng = Route.first.stops.first.stop_lon
-	@routelast_lat = Route.first.stops.last.stop_lat
-	@routelast_lng = Route.first.stops.last.stop_lon
 
-	
+  # @route1 = Route.find(params[:id])
+  # @route_end = route1.route_stops.last.stop
+
 
   end
 end
